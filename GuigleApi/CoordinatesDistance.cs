8d6df32b617516cb67c1872f533d244da16f0f97
Gui <Gui>
2020-01-27 19:24:50 +1000

    Added Travel() to CoordinatesDistance static helper

---
+++ GuigleApi/CoordinatesDistance.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using GuigleApi.Models.Address;
+using GuigleApi.Models.Coordinates;
+
+namespace GuigleApi
+{
+    public static class CoordinatesDistance
+    {
+        public static double DistanceTo(this Coordinates baseCoordinates, Coordinates targetCoordinates)
+        {
+            return DistanceTo(baseCoordinates, targetCoordinates, UnitOfLength.Kilometers);
+        }
+
+        public static double DistanceTo(this Coordinates baseCoordinates, Coordinates targetCoordinates, UnitOfLength unitOfLength)
+        {
+            var baseRad = Math.PI * baseCoordinates.Latitude / 180;
+            var targetRad = Math.PI * targetCoordinates.Latitude / 180;
+            var theta = baseCoordinates.Longitude - targetCoordinates.Longitude;
+            var thetaRad = Math.PI * theta / 180;
+
+            double dist =
+                Math.Sin(baseRad) * Math.Sin(targetRad) + Math.Cos(baseRad) *
+                Math.Cos(targetRad) * Math.Cos(thetaRad);
+            dist = Math.Acos(dist);
+
+            dist = dist * 180 / Math.PI;
+            dist = dist * 60 * 1.1515;
+
+            return unitOfLength.ConvertFromMiles(dist);
+        }
+
+        public static double EarhRadiusInKm { get; } = 6371;
+
+        /// <summary>
+        /// Gets the rounded distance in kilometers
+        /// </summary>
+        /// <param name="lat1"></param>
+        /// <param name="lat2"></param>
+        /// <param name="lng1"></param>
+        /// <param name="lng2"></param>
+        /// <returns></returns>
+        public static int GetDistance(double lat1, double lat2, double lng1, double lng2)
+        {
+            return Convert.ToInt32(Math.Round(GetPreciseDistance(lat1, lat2, lng1, lng2)));
+        }
+
+        /// <summary>
+        /// Gets the precise distance in kilometers
+        /// </summary>
+        /// <param name="lat1"></param>
+        /// <param name="lat2"></param>
+        /// <param name="lng1"></param>
+        /// <param name="lng2"></param>
+        /// <returns></returns>
+        public static double GetPreciseDistance(double lat1, double lat2, double lng1, double lng2)
+        {
+            var distance = new Coordinates(lat1, lng1)
+                .DistanceTo(
+                    new Coordinates(lat2, lng2),
+                    UnitOfLength.Kilometers
+                );
+
+            return distance;
+        }
+
+        public static Location Travel(Location startLocation, double initialDegree, double distanceInKm)
+        {
+            double ib = DegreesToRadians(initialDegree);
+            double startLat1Rad = DegreesToRadians(startLocation.Lat);
+            double startLon1Rad = DegreesToRadians(startLocation.Lng);
+            double distRad = distanceInKm / EarhRadiusInKm ;
+
+            double a = Math.Sin(distRad) * Math.Cos(startLat1Rad);
+            double lat2 = Math.Asin(Math.Sin(startLat1Rad) * Math.Cos(distRad) + a * Math.Cos(ib));
+            double lon2 = startLon1Rad + Math.Atan2(Math.Sin(ib) * a, Math.Cos(distRad) - Math.Sin(startLat1Rad) * Math.Sin(lat2));
+
+            return new Location(RadiansToDegrees(lat2), RadiansToDegrees(lon2));
+        }
+
+        public static double DegreesToRadians(double angle)
+        {
+            return (Math.PI / 180) * angle;
+        }
+
+        public static double RadiansToDegrees(double radians)
+        {
+            return radians * (180.0 / Math.PI);
+        }
+    }
+}
---
+++ GuigleApiXUnitTest/CoordinatesTest.cs
@@ -0,0 +1,37 @@
+using GuigleApi;
+using GuigleApi.Models.Address;
+using Xunit;
+
+namespace GuigleApiXUnitIntegrationTest
+{
+    public class CoordinatesTest
+    {
+        private readonly Location _placeLocation1 = new Location(-27.4703967, 153.0042494);
+
+        [Fact]
+        public void GetDistance_Travel_North_Should_Match()
+        {
+            var distanceInKm = 1;
+            var directionDegree = 0; // North
+            
+            var travelLocation = CoordinatesDistance.Travel(_placeLocation1, directionDegree, distanceInKm);
+
+            var distanceResult = CoordinatesDistance.GetDistance(_placeLocation1.Lat, travelLocation.Lat, _placeLocation1.Lng, travelLocation.Lng);
+
+            Assert.Equal(distanceInKm, distanceResult);
+        }
+
+        [Fact]
+        public void GetDistance_Travel_South_Should_Match()
+        {
+            var distanceInKm = 1;
+            var directionDegree = 180; // South
+
+            var travelLocation = CoordinatesDistance.Travel(_placeLocation1, directionDegree, distanceInKm);
+
+            var distanceResult = CoordinatesDistance.GetDistance(_placeLocation1.Lat, travelLocation.Lat, _placeLocation1.Lng, travelLocation.Lng);
+
+            Assert.Equal(distanceInKm, distanceResult);
+        }
+    }
+}
